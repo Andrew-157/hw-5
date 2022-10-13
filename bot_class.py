acb2ef6d57f38cf4a3d48800263012f5394aaceb
Andrew Subotin <subotinandrey5@gmail.com>
2022-10-13 13:18:50 +0300

    Update bot_class.py

--- bot_class.py
+++ bot_class.py
@@ -7,30 +7,42 @@ class Field:
 
 class Name:
     def __init__(self, name):
-        self.name = name
+        self.value = name
 
 
 class Phone:
     def __init__(self, phone):
-        self.phone = phone
+        self.value = phone
 
 
 class Record:
-    name = ""
-    phone = ""
-    Name = Name(name)
-    Phone = Phone(phone)
+    def __init__(self, name=None, phone=None):
+        if phone == None:
+            self.name = Name(name)
+        else:
+            self.name = Name(name)
+            self.phone = Phone(phone)
 
-    def delete(self, value):
-        address_book.data.pop(value)
+    def __str__(self):
+        try:
+            return f"{self.name.value}--{self.phone.value}"
+        except AttributeError:
+            return f"{self.name.value}"
 
-    def change(self, key, value):
-        address_book[key] = value
+    def change(self, name, info):
+        record = Record(name, info)
+        address_book.data[record.name.value] = record.__str__()
+
+    def delete(self, name):
+        address_book.data.pop(name)
+
+    def delete_all(self):
+        address_book.data.clear()
 
 
 class AddressBook(UserDict):
     def add_record(self, record):
-        self.data[record.name] = record.phone
+        self.data[record.name.value] = record.__str__()
         return self.data
 
 
@@ -59,21 +71,26 @@ def say_hello():
 
 @input_error
 def add(data):
-    name_ = data.strip().split(" ")[1]
-    phone_ = data.strip().split(" ")[2]
-    record = Record()
-    record.name = name_
-    record.phone = phone_
+    if len(data.split(" ")) == 2:
 
-    if name_ in address_book.data:
-        return f"{name_} already has a number, call change function"
+        name_ = data.split(" ")[1]
+        if name_ in address_book.data:
+            return f"{name_} already has information, if you want to change it,call 'change' command."
 
-    elif not phone_.isnumeric():
-        return f"{phone_} isn't a right input,enter a numeric one"
+        record = Record(name_)
+        address_book.add_record(record)
+        return f"{name_} was added"
 
     else:
+
+        name_ = data.split(" ")[1]
+        if name_ in address_book.data:
+            return f"{name_} already has information, if you want to change it,call 'change' command."
+
+        info = data.split(" ")[2:]
+        record = Record(name_, info)
         address_book.add_record(record)
-        return f"User {name_} with {phone_} was added."
+        return f"{name_} with {info} was added"
 
 
 def show_all():
@@ -81,11 +98,11 @@ def show_all():
 
 
 @input_error
-def phone(data):
+def info(data):
     name_ = data.strip().split(" ")[1]
 
     if name_ not in address_book.data:
-        return f"{name_} doesn't have a number, call 'add' command to add this user"
+        return f"{name_} doesn't have any information, call 'add' command to add {name_}"
     else:
         return address_book[name_]
 
@@ -94,30 +111,34 @@ def phone(data):
 def delete(data):
     name_ = data.strip().split(" ")[1]
 
-    if name_ not in address_book.data:
-        return f"{name_} doesn't have a number, you can't delete it"
+    if name_.lower() == "all":
+        Record().delete_all()
+        return "All information was deleted."
+    elif name_ not in address_book.data:
+        return f"{name_} doesn't have any information, you can't delete it"
     else:
         Record().delete(name_)
-        return f"User {name_} was deleted"
+        return f"{name_} was deleted"
 
 
 @input_error
 def change(data):
-    name_ = data.strip().split(" ")[1]
-    phone_ = data.strip().split(" ")[2]
+    name_ = data.split(" ")[1]
 
     if name_ not in address_book.data:
-        return f"{name_} doesn't have a number, call 'add' command to add this user"
-    elif not phone_.isnumeric():
-        return f"{phone_} isn't a right input,enter a numeric one"
+        return f"{name_} doesn't have any information "
     else:
-        Record().change(name_, phone_)
-        return f"{name_}'s phone number was changed to {phone_}"
+        if len(data.split(" ")) < 3:
+            return "Enter the name with its new values"
+        else:
+            info = data.split(" ")[2:]
+            Record().change(name_, info)
+            return f"{name_}'s information was changed to {info}"
 
 
 COMMANDS = {"hello": say_hello,
             "add": add,
-            "phone": phone,
+            "info": info,
             "change": change,
             "show all": show_all,
             "delete": delete}
@@ -134,7 +155,7 @@ def main():
         if user_command.lower() == "hello" or user_command.lower() == "show all":
             print(handler(user_command.lower())())
 
-        elif user_command.split(" ")[0].lower() not in ["change", "phone", "add", "exit", "close", "goodbye", "delete"]:
+        elif user_command.split(" ")[0].lower() not in ["change", "info", "add", "exit", "close", "goodbye", "delete"]:
             print("No such a command")
 
         elif user_command.lower() in ["exit", "close", "goodbye"]:
